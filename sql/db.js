1da0bc74d7bf3a502b2c7e53bef2615c8d88e56f
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2023-08-01 16:03:26 +0700

    Some cart

--- sql/db.js
+++ sql/db.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 import mysql from 'mysql2/promise';
 
 dotenv.config();
-// export const db = await mysql.createConnection(process.env.DATABASE_URL);
-export const db = await mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
-});
+export const db = await mysql.createConnection(process.env.DATABASE_URL);
+// export const db = await mysql.createConnection({
+//     host: process.env.MYSQLHOST,
+//     port: process.env.MYSQLPORT,
+//     user: process.env.MYSQLUSER,
+//     password: process.env.MYSQLPASSWORD,
+//     database: process.env.MYSQLDATABASE
+// });
 console.log('Connected to PlanetScale!')
